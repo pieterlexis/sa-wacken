1a9cd43b02048409828c599d12d93a9085cf8623
Pieter Lexis <pieter@plexis.eu>
2015-08-14 15:18:45 +0200

    Use a generic error function, try to connect to a bunch of ports (first on wins)

--- index.py
+++ index.py
@@ -2,7 +2,6 @@ import web
 from web.contrib.template import render_jinja
 from datetime import datetime
 from datetime import timedelta
-import threading
 import serial
 import re
 import random
@@ -22,9 +21,44 @@ change_time = datetime.now() - timedelta(seconds=min_ttl)
 render = render_jinja('templates', encoding='utf-8')
 app = web.application(urls, globals())
 session = web.session.Session(app, web.session.DiskStore('sessions'))
+color_chars = "1234567890abcdef"
+ser = None
 
-ser = serial.Serial('/dev/ttyUSB0')
-color_chars="1234567890abcdef"
+
+def try_connect():
+    global ser
+    for t in ['USB', 'ACM']:
+        for n in range(0, 20):
+            try:
+                ser = serial.Serial('/dev/tty%s%s' % (t, n))
+                return
+            except:
+                pass
+
+
+def sendit(msg):
+    global ser
+    ctr = 0
+
+    if ser is None:
+        try_connect()
+
+    ctr = 0
+    while ctr < 4:
+        ctr = ctr + 1
+        try:
+            ser.write(toSend)
+            return True
+        except:
+            ser.close()
+            try_connect()
+
+    return False
+
+
+def doError(msg):
+    session.error = msg
+    return web.SeeOther('/')
 
 
 class index:
@@ -56,6 +90,7 @@ class index:
                            green=g,
                            blue=b)
 
+
 class admin:
     def GET(self):
         return "Nothing to see here"
@@ -72,53 +107,41 @@ class arduino_change:
         global ser
         s = web.input()
         if kind in kinds:
-            seconds_passed= (datetime.now()-change_time).total_seconds()
+            seconds_passed = (datetime.now()-change_time).total_seconds()
             if seconds_passed > min_ttl:
                 toSend = ""
                 toSend += chr(kinds.index(kind))
                 if kind in ['colorwipe', 'theater', 'knightrider']:
                     if 'color' in s and re.match(colorRegex, s['color']):
-                        for i in [1,3,5]:
+                        for i in [1, 3, 5]:
                             toSend += chr(int(s['color'][i]+s['color'][i+1], 16))
                 else:
                     for x in range(3):
                         toSend += chr(0)
-
 
                 if 'speed' in s and 1 < int(s['speed']) < 10:
                     toSend += chr((11 - int(s['speed']))*10)
                 else:
                     toSend += chr(5)
 
-
                 if len(toSend) != 5:
-                    session.error = "Something went seriously wrong :("
-                    return web.SeeOther('/')
+                    doError("Something went seriously wrong :(")
 
-                ctr = 0
-
-                while ctr < 4:
-                    ctr = ctr + 1
-                    try:
-                        ser.write(toSend)
-                        break
-                    except:
-                        ser.close()
-                        ser = serial.Serial('/dev/ttyUSB0')
+                if not sendit(toSend):
+                    doError("<strong>Unable to contact the controller</strong>")
 
                 change_time = datetime.now()
                 return web.SeeOther('/')
 
             else:
                 s = 'second' if int(seconds_passed) == 1 else 'seconds'
-                session.error="<strong>Last change was made %s %s ago.</strong> Try later." % (int(seconds_passed), s)
+                session.error = "<strong>Last change was made %s %s ago.</strong> Try later." % (int(seconds_passed), s)
                 return web.SeeOther('/')
 
         else:
             raise web.NotFound('aaaaah')
 
-
-    def GET(self,kind):
+    def GET(self, kind):
         web.redirect(web.ctx.home)
 
 if __name__ == "__main__":
